9c28211ecd1407970f20fe5baab1e533b2099183
Josh Beard <joshuabeard92@gmail.com>
2019-03-27 15:11:36 -0400

    pip-installable!

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='netdev',
+      version='0.0.0',
+      description='A collection of tools to make neural network development faster, easier, and more readable',
+      long_description=long_description,
+      author='Joshua Beard',
+      author_email='joshuabeard92@gmail.com',
+      packages=find_packages(),
+      install_requires=['numpy', 'torch', 'ubelt', 'ipdb', 'IPython', 'torchvision'],
+      url='https://github.com/JoshuaBeard/netdev',
+      changelog={'0.0.0': 'Beta',
+                 }
+      )
